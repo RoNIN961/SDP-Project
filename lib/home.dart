a2493f5d60b7dbe1f39d56777f27376c6e6117fd
RoNIN961 <75106650+RoNIN961@users.noreply.github.com>
2020-12-03 01:48:06 +0800

    Update home.dart

--- lib/home.dart
+++ lib/home.dart
@@ -20,9 +20,14 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('My Flutter App'),
-      ),
+      appBar: AppBar(title: Text('My Flutter App'), actions: <Widget>[
+        FlatButton(
+          onPressed: () {
+            /*...*/
+          },
+          child: Text("Check Points"),
+        )
+      ]),
       body: _children[_currentIndex], // new
       bottomNavigationBar: BottomNavigationBar(
         onTap: onTabTapped, // new
