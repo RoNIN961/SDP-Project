ee3257f78af47880076dc017f7a112f9a8922a07
RoNIN961 <75106650+RoNIN961@users.noreply.github.com>
2020-12-06 22:31:59 +0800

    Update homecontent.dart

--- lib/homecontent.dart
+++ lib/homecontent.dart
@@ -3,26 +3,33 @@ import 'package:flutter/material.dart';
 class HomeContentPage extends StatelessWidget {
 @override
 Widget build(BuildContext context) {
-  Card(
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
-          const ListTile(
-            title: Text('Recipe Name'),
-            subtitle: Text('Author Name'),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: <Widget>[
-              TextButton(
-                child: const Text('View'),
-                onPressed: () {/* ... */},
-              ),
-              const SizedBox(width: 8),
-            ],
-          ),
-        ],
+  return Scaffold(
+    body: SafeArea(
+    child: Column(
+      children: [
+        Card(
+          child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            const ListTile(
+              title: Text('Recipe Name'),
+              subtitle: Text('Author Name'),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[
+                TextButton(
+                 child: const Text('View'),
+                  onPressed: () {/* ... */},
+                ),
+                const SizedBox(width: 8),
+              ],
+            ),
+          ],
+        ),
       ),
     ),
+    ),
+  ),
 }
 }
