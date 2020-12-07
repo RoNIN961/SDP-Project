5a77bbc6be3c1874ff4c5c2d9184d9498d1abb4c
RoNIN961 <75106650+RoNIN961@users.noreply.github.com>
2020-12-07 17:23:08 +0800

    Update recipe_detail.dart

--- lib/recipe_detail.dart
+++ lib/recipe_detail.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
+import 'home.dart';
 
-class RecipeScreen extends StatelessWidget {
+class RecipeScreen extends StatefulWidget {
+  @override
+  _RecipeScreenState createState() => _RecipeScreenState();
+}
+
+class _RecipeScreenState extends State<RecipeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
