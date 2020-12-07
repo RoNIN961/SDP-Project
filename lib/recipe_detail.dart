17f492868321665e57de71e1bc74a63b53562a93
RoNIN961 <75106650+RoNIN961@users.noreply.github.com>
2020-12-07 14:14:09 +0800

    Update recipe_detail.dart

--- lib/recipe_detail.dart
+++ lib/recipe_detail.dart
@@ -3,37 +3,15 @@ import 'package:flutter/material.dart';
 class RecipeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        Expanded(
-          flex: 2,
-          child: Container(
-              padding: EdgeInsets.all(20),
-              width: double.infinity,
-              child: Text(
-                "Recipe Name ",
-                textAlign: TextAlign.center,
-              )),
-        ),
-        Expanded(
-          flex: 6,
-          child: Container(
-            width: double.infinity,
-            child: Image.asset(
-              'logo.png',
-              fit: BoxFit.fill,
-            ),
-          ),
-        ),
-        Expanded(
-          flex: 6,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[Text('Ingredient 1'), Text('Ingredient 2')],
-          ),
-        ),
-      ],
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          Text('Name'),
+          Image.asset('logo.png'),
+          Text('Ingredients'),
+          Text('Steps'),
+        ],
+      ),
     );
   }
 }
