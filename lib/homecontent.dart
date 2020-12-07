a6a5c80b6d948c06603472668b6f32887c38d273
RoNIN961 <75106650+RoNIN961@users.noreply.github.com>
2020-12-07 18:01:25 +0800

    updates

--- lib/homecontent.dart
+++ lib/homecontent.dart
@@ -2,39 +2,41 @@ import 'package:flutter/material.dart';
 import 'recipe_detail.dart';
 
 class HomeContentPage extends StatelessWidget {
-@override
-Widget build(BuildContext context) {
-  return Scaffold(
-    body: SafeArea(
-    child: Column(
-      children: [
-        Card(
-          child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            const ListTile(
-              title: Text('Recipe Name'),
-              subtitle: Text('Author Name'),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: <Widget>[
-                TextButton(
-                 child: const Text('View'),
-                  onPressed: () {
-                    Navigator.pushReplacement(
-                      context, MaterialPageRoute(builder: (context) => RecipeScreen()));
-              },
-                ),
-                const SizedBox(width: 8),
-              ],
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Card(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  const ListTile(
+                    title: Text('Recipe Name'),
+                    subtitle: Text('Author Name'),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: <Widget>[
+                      TextButton(
+                        child: const Text('View'),
+                        onPressed: () {
+                          Navigator.pushReplacement(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => RecipeScreen()));
+                        },
+                      ),
+                      const SizedBox(width: 8),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ],
         ),
       ),
-      ],
-    ),
-    ),
-  );
-}
+    );
+  }
 }
--- lib/recipe_detail.dart
+++ lib/recipe_detail.dart
@@ -19,11 +19,8 @@ class _RecipeScreenState extends State<RecipeScreen> {
           Container(
             width: 80.0,
             height: 80.0,
-            child: FittedBox(
-              fit: BoxFit.contain,
-              child: Image.network(
-                  'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.thestar.com.my%2Ffood%2Feating-out%2F2020%2F02%2F28%2Fenjoy-value-for-money-pizza&psig=AOvVaw2GvEReG91Ytq38g_H_OaBm&ust=1607414307412000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCKjgjomzu-0CFQAAAAAdAAAAABAJ'),
-            ),
+            child: Image.network(
+                'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.thestar.com.my%2Ffood%2Feating-out%2F2020%2F02%2F28%2Fenjoy-value-for-money-pizza&psig=AOvVaw2GvEReG91Ytq38g_H_OaBm&ust=1607414307412000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCKjgjomzu-0CFQAAAAAdAAAAABAJ'),
           ),
           Text('Ingredients'),
           Text('Steps'),
