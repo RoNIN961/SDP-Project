589923ac5733796282b659229741b27c13e47f25
RoNIN961 <75106650+RoNIN961@users.noreply.github.com>
2020-12-08 15:02:41 +0800

    Update search.dart

--- lib/search.dart
+++ lib/search.dart
@@ -1,20 +1,31 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
 
 class SearchPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('My Flutter App'), actions: <Widget>[
-        FlatButton(
-          onPressed: () {},
-          child: Text("Check Points"),
-        )
-      ]),
-      body: Container(
-        searchbar : SearchBar('press here');
-      )
-    );
+        appBar: AppBar(title: Text('My Flutter App'), actions: <Widget>[
+          FlatButton(
+            onPressed: () {},
+            child: Text("Check Points"),
+          )
+        ]),
+        body: Container(
+            child: Column(
+          children: <Widget>[
+            Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextField(
+                  onChanged: (value) {},
+                  controller: TextEditingController(),
+                  decoration: InputDecoration(
+                      labelText: 'Search',
+                      hintText: 'Search',
+                      prefixIcon: Icon(Icons.search),
+                      border: OutlineInputBorder(
+                          borderRadius: Border.all(Radius.circular(25.0)))),
+                ))
+          ],
+        )));
   }
 }
