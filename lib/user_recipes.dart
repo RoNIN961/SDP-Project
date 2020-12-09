30b8c59ed82193c7da6e10d315271747c467fed6
RoNIN961 <75106650+RoNIN961@users.noreply.github.com>
2020-12-10 02:29:49 +0800

    Update user_recipes.dart

--- lib/user_recipes.dart
+++ lib/user_recipes.dart
@@ -19,33 +19,40 @@ class _UserRecipePageState extends State<UserRecipePage> {
 
   Widget build(BuildContext context) {
     return Scaffold(
+        appBar: AppBar(title: Text('My Flutter App'), actions: <Widget>[
+          FlatButton(
+            onPressed: () {},
+            child: Text("Check Points"),
+          )
+        ]),
         body: Container(
             child: Column(
-      children: <Widget>[
-        Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextField(
-              onChanged: (value) {},
-              controller: TextEditingController(),
-              decoration: InputDecoration(
-                  labelText: 'Search',
-                  hintText: 'Search',
-                  prefixIcon: Icon(Icons.search),
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(25.0)))),
-            )),
-        Expanded(
-          child: ListView.builder(
-            shrinkWrap: true,
-            itemCount: items.length,
-            itemBuilder: (context, index) {
-              return ListTile(
-                title: Text('${items[index]}'),
-              );
-            },
-          ),
-        ),
-      ],
-    )));
+          children: <Widget>[
+            Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextField(
+                  onChanged: (value) {},
+                  controller: TextEditingController(),
+                  decoration: InputDecoration(
+                      labelText: 'Search',
+                      hintText: 'Search',
+                      prefixIcon: Icon(Icons.search),
+                      border: OutlineInputBorder(
+                          borderRadius:
+                              BorderRadius.all(Radius.circular(25.0)))),
+                )),
+            Expanded(
+              child: ListView.builder(
+                shrinkWrap: true,
+                itemCount: items.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text('${items[index]}'),
+                  );
+                },
+              ),
+            ),
+          ],
+        )));
   }
 }
