af945c507189ebf0e423e8ad572add9a28200110
RoNIN961 <75106650+RoNIN961@users.noreply.github.com>
2020-12-09 18:52:42 +0800

    Update edit_profile.dart

--- lib/edit_profile.dart
+++ lib/edit_profile.dart
@@ -8,6 +8,26 @@ class EditProfilePage extends StatefulWidget {
 class _EditProfilePageState extends State<EditProfilePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Container());
+    return Scaffold(
+        body: SafeArea(
+            child: Column(children: [
+      Container(
+        alignment: Alignment(0.0, 2.5),
+        child: CircleAvatar(
+          backgroundImage: NetworkImage("Add you profile DP image URL here "),
+          radius: 60.0,
+        ),
+      ),
+      FlatButton(
+          onPressed: () {},
+          child: Text(
+            'Change Your Profile Picture',
+            style: TextStyle(
+                fontSize: 25.0,
+                color: Colors.blueAccent,
+                letterSpacing: 2.0,
+                fontWeight: FontWeight.w400),
+          ))
+    ])));
   }
 }
