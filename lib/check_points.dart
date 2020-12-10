c1f70a1183e8898032ddcd4437fe145898c65862
RoNIN961 <75106650+RoNIN961@users.noreply.github.com>
2020-12-10 14:07:47 +0800

    Update check_points.dart

--- lib/check_points.dart
+++ lib/check_points.dart
@@ -1,5 +1,16 @@
 import 'package:flutter/material.dart';
+import 'home.dart';
 
 class CheckPointsPage extends StatelessWidget {
-
+  @override
+  Widget build(BuildContext context) {
+    AppBar(
+      leading: BackButton(
+        onPressed: () {
+          Navigator.pushReplacement(
+              context, MaterialPageRoute(builder: (context) => Home()));
+        },
+      ),
+    );
+  }
 }
