56b95841d244461df4667fd171dce8e0a7fcea4d
RoNIN961 <75106650+RoNIN961@users.noreply.github.com>
2020-12-10 14:09:42 +0800

    Update check_points.dart

--- lib/check_points.dart
+++ lib/check_points.dart
@@ -4,13 +4,20 @@ import 'home.dart';
 class CheckPointsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    AppBar(
-      leading: BackButton(
-        onPressed: () {
-          Navigator.pushReplacement(
-              context, MaterialPageRoute(builder: (context) => Home()));
-        },
-      ),
-    );
+    return Scaffold(
+        appBar: AppBar(
+          leading: BackButton(
+            onPressed: () {
+              Navigator.pushReplacement(
+                  context, MaterialPageRoute(builder: (context) => Home()));
+            },
+          ),
+        ),
+        body: Container(
+          margin: EdgeInsets.all(100.0),
+          decoration:
+              BoxDecoration(color: Colors.orange, shape: BoxShape.circle),
+          child: Text('Check your points here'),
+        ));
   }
 }
