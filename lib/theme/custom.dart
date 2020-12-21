156591b95138207ac54997812750bf99c8ad9c63
RoNIN961 <75106650+RoNIN961@users.noreply.github.com>
2020-12-22 04:21:47 +0800

    Create custom.dart

---
+++ lib/theme/custom.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
+
+class CustomTextField extends StatelessWidget {
+  CustomTextField({@required this.onPressed});
+  final GestureTapCallback onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return RawMaterialButton(
+      child: Padding(
+        padding: EdgeInsets.only(bottom: 10),
+        child: TextField(
+          keyboardType: TextInputType.emailAddress,
+          decoration: InputDecoration(
+              hintText: 'text',
+              contentPadding:
+                  EdgeInsets.symmetric(horizontal: 25, vertical: 20),
+              border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(50.0))),
+        ),
+      ),
+      onPressed: onPressed,
+    );
+  }
+}
+
+class CustomButton extends StatelessWidget {
+  CustomButton({@required this.onPressed});
+  final GestureTapCallback onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return RawMaterialButton(
+      child: Padding(
+        padding: EdgeInsets.only(bottom: 5),
+        child: ButtonTheme(
+          height: 56,
+          child: RaisedButton(
+            child: Text('Text',
+                style: TextStyle(color: Colors.white, fontSize: 20)),
+            color: Colors.black87,
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(50)),
+            onPressed: () {},
+          ),
+        ),
+      ),
+      onPressed: onPressed,
+    );
+  }
+}
