815968e4685260a1b0265c87de6b98fe9af0ad6d
RoNIN961 <75106650+RoNIN961@users.noreply.github.com>
2020-12-22 16:29:16 +0800

    Update custom.dart

--- lib/theme/custom.dart
+++ lib/theme/custom.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart';
 
 gettheme() {
-  return {ThemeData(primaryColor: Colors.orangeAccent)};
+  return {ThemeData(primaryColor: Colors.deepOrange)};
 }
 
 class CustomLogo extends StatelessWidget {
