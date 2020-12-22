94151a9dfe0fdd3231a3f2fc196431f83ff341a6
RoNIN961 <75106650+RoNIN961@users.noreply.github.com>
2020-12-22 15:41:04 +0800

    Update reset_password.dart

--- lib/screens/authentication/reset_password.dart
+++ lib/screens/authentication/reset_password.dart
@@ -4,11 +4,19 @@ import 'package:sdp_project/theme/custom.dart';
 class ResetPasswordPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: Center(
-            child: CustomTextField(
-      onPressed: null,
-      text: 'email address',
-    )));
+    final inputEmail = CustomTextField(onPressed: null, text: 'email address');
+    final buttonReset = CustomButton2(
+        onPressed: () {
+          Navigator.pushNamed(context, '/');
+        },
+        text: 'Reset Password');
+
+    return SafeArea(
+        child: Scaffold(
+      body: Center(
+          child: ListView(
+        children: <Widget>[inputEmail, buttonReset],
+      )),
+    ));
   }
 }
