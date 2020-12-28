86d4b79ed619f17ba69880b4f099c6926f5caee0
RoNIN961 <75106650+RoNIN961@users.noreply.github.com>
2020-12-29 01:52:28 +0800

    Update homecontent.dart

--- lib/screens/homescreen/homecontent.dart
+++ lib/screens/homescreen/homecontent.dart
@@ -1,38 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:sdp_project/theme/custom.dart';
 
 class HomeContentPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Column(
-          children: [
-            Card(
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: <Widget>[
-                  const ListTile(
-                    title: Text('Recipe Name'),
-                    subtitle: Text('Author Name'),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: <Widget>[
-                      TextButton(
-                        child: const Text('View'),
-                        onPressed: () {
-                          Navigator.pushNamed(context, '/recipe_detail');
-                        },
-                      ),
-                      const SizedBox(width: 8),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
-      ),
+    return ListView(
+      children: <Widget>[CustomRecipeListView(), CustomRecipeListView()],
     );
   }
 }
