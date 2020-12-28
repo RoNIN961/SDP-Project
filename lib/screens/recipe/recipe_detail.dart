8b9d3235b8cce517d9936aff1954151007abb90f
RoNIN961 <75106650+RoNIN961@users.noreply.github.com>
2020-12-29 04:47:49 +0800

    Update recipe_detail.dart

--- lib/screens/recipe/recipe_detail.dart
+++ lib/screens/recipe/recipe_detail.dart
@@ -9,31 +9,69 @@ class _RecipeScreenState extends State<RecipeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-          leading: BackButton(
-            onPressed: () {
-              Navigator.pushNamed(context, '/home');
-            },
+        appBar: AppBar(
+            leading: BackButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/home');
+              },
+            ),
+            title: Text('My Flutter App'),
+            actions: <Widget>[]),
+        body: Container(
+          child: Column(
+            children: <Widget>[
+              Title(color: Colors.black, child: Text('Title')),
+              SizedBox(
+                width: 400.0,
+                height: 200.0,
+                child: Image(image: null),
+              ),
+              Container(
+                child: ListView(
+                  children: <Widget>[
+                    Title(color: Colors.black, child: Text('Ingredients')),
+                    ListTile(
+                      title: Text('Ingredient 1'),
+                    ),
+                    ListTile(
+                      title: Text('Ingredient 2'),
+                    ),
+                    ListTile(
+                      title: Text('Ingredient 3'),
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                child: ListView(
+                  children: <Widget>[
+                    Title(color: Colors.black, child: Text('Steps')),
+                    ListTile(
+                      title: Text('Step 1'),
+                    ),
+                    ListTile(
+                      title: Text('Step 2'),
+                    ),
+                    ListTile(
+                      title: Text('Step 3'),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                  child: Column(children: <Widget>[
+                CircleAvatar(
+                  child: Image(image: null),
+                ),
+                Text('Published by'),
+                Text(
+                  'Author Name',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30.0),
+                ),
+                Text('on "Date Published"')
+              ]))
+            ],
           ),
-          title: Text('My Flutter App'),
-          actions: <Widget>[]),
-      body: ListView(
-        children: <Widget>[
-          Center(
-              child: Text(
-            'Name',
-            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 50),
-          )),
-          Container(
-            width: 80.0,
-            height: 80.0,
-            child: Image.network(
-                'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.thestar.com.my%2Ffood%2Feating-out%2F2020%2F02%2F28%2Fenjoy-value-for-money-pizza&psig=AOvVaw2GvEReG91Ytq38g_H_OaBm&ust=1607414307412000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCKjgjomzu-0CFQAAAAAdAAAAABAJ'),
-          ),
-          Text('Ingredients'),
-          Text('Steps'),
-        ],
-      ),
-    );
+        ));
   }
 }
