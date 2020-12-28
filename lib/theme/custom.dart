caeba40b82d415ce788f1a2d0b8126897f069655
RoNIN961 <75106650+RoNIN961@users.noreply.github.com>
2020-12-29 01:29:54 +0800

    Update custom.dart

--- lib/theme/custom.dart
+++ lib/theme/custom.dart
@@ -183,7 +183,212 @@ class CustomDrawer extends StatelessWidget {
   }
 }
 
-class CustomRecipeListView {
+class CustomRecipeListView extends StatelessWidget {
   @override
-  Widget build(Build)
+  Widget build(BuildContext context) {
+    return Container(
+      height: 200.0,
+      decoration: new BoxDecoration(boxShadow: [
+        new BoxShadow(
+          color: Color(0xffeeeeee),
+          blurRadius: 1.0,
+          offset: new Offset(1.0, 1.0),
+        ),
+      ]),
+      child: Card(
+        child: InkWell(
+            child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(left: 10.0, top: 10.0),
+              child: Text(
+                "Welcome to FoodAmore",
+                style: TextStyle(color: Color(0xff616161), fontSize: 16.0),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 10.0, top: 3.0),
+              child: Text(
+                "Browse our most popular recipes",
+                style: TextStyle(color: Color(0xff616161), fontSize: 12.0),
+              ),
+            ),
+            Expanded(
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.only(
+                        top: 15.00, bottom: 15.00, left: 8, right: 4),
+                    child: Container(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Image(
+                            image: AssetImage(''),
+                            width: 80.0,
+                            height: 80.0,
+                          ),
+                          Padding(
+                            padding: EdgeInsets.all(2.0),
+                          ),
+                          Text(
+                            "Recipe Name",
+                            style: TextStyle(
+                              color: Color(0xff202124),
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.all(2.0),
+                          ),
+                          Text(
+                            "Author name",
+                            style: TextStyle(
+                                color: Color(0xff5f6368), fontSize: 12.0),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(
+                        top: 15.00, bottom: 15.00, left: 8, right: 4),
+                    child: Container(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Image(
+                            image: AssetImage(''),
+                            width: 80.0,
+                            height: 80.0,
+                          ),
+                          Padding(
+                            padding: EdgeInsets.all(2.0),
+                          ),
+                          Text(
+                            "Recipe Name",
+                            style: TextStyle(
+                              color: Color(0xff202124),
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.all(2.0),
+                          ),
+                          Text(
+                            "Author name",
+                            style: TextStyle(
+                                color: Color(0xff5f6368), fontSize: 12.0),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(
+                        top: 15.00, bottom: 15.00, left: 8, right: 4),
+                    child: Container(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Image(
+                            image: AssetImage(''),
+                            width: 80.0,
+                            height: 80.0,
+                          ),
+                          Padding(
+                            padding: EdgeInsets.all(2.0),
+                          ),
+                          Text(
+                            "Recipe Name",
+                            style: TextStyle(
+                              color: Color(0xff202124),
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.all(2.0),
+                          ),
+                          Text(
+                            "Author name",
+                            style: TextStyle(
+                                color: Color(0xff5f6368), fontSize: 12.0),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(
+                        top: 15.00, bottom: 15.00, left: 8, right: 4),
+                    child: Container(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Image(
+                            image: AssetImage(''),
+                            width: 80.0,
+                            height: 80.0,
+                          ),
+                          Padding(
+                            padding: EdgeInsets.all(2.0),
+                          ),
+                          Text(
+                            "Recipe Name",
+                            style: TextStyle(
+                              color: Color(0xff202124),
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.all(2.0),
+                          ),
+                          Text(
+                            "Author name",
+                            style: TextStyle(
+                                color: Color(0xff5f6368), fontSize: 12.0),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(
+                        top: 15.00, bottom: 15.00, left: 8, right: 4),
+                    child: Container(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Image(
+                            image: AssetImage(''),
+                            width: 80.0,
+                            height: 80.0,
+                          ),
+                          Padding(
+                            padding: EdgeInsets.all(2.0),
+                          ),
+                          Text(
+                            "Recipe Name",
+                            style: TextStyle(
+                              color: Color(0xff202124),
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.all(2.0),
+                          ),
+                          Text(
+                            "Author name",
+                            style: TextStyle(
+                                color: Color(0xff5f6368), fontSize: 12.0),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        )),
+      ),
+    );
+  }
 }
