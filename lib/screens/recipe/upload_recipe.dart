104af18688b3d093b8bafbd0d43e38c9161757a6
RoNIN961 <75106650+RoNIN961@users.noreply.github.com>
2020-12-29 04:34:58 +0800

    Update upload_recipe.dart

--- lib/screens/recipe/upload_recipe.dart
+++ lib/screens/recipe/upload_recipe.dart
@@ -4,32 +4,29 @@ class UploadRecipePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(actions: <Widget>[
-          FlatButton(onPressed: null, child: Text('Upload'))
-        ]),
         body: Container(
             child: ListView(
-          children: [
-            SizedBox(
-                width: 400.0,
-                height: 80.0,
-                child: InkWell(
-                  child: Text('Upload Recipe Image'),
-                  onTap: () {},
-                )),
-            TextField(
-              decoration: InputDecoration(hintText: 'Title:Ginger Chicken'),
-            ),
-            Container(
-                child: Column(
-              children: <Widget>[
-                Title(color: Colors.black, child: Text('Ingredients')),
-                TextField(decoration: InputDecoration(hintText: '250g flour')),
-                TextField(decoration: InputDecoration(hintText: '100ml water')),
-                RaisedButton(child: Text('Add Ingredient'), onPressed: null)
-              ],
-            ))
+      children: [
+        SizedBox(
+            width: 400.0,
+            height: 80.0,
+            child: InkWell(
+              child: Text('Upload Recipe Image'),
+              onTap: () {},
+            )),
+        TextField(
+          decoration: InputDecoration(hintText: 'Title:Ginger Chicken'),
+        ),
+        Container(
+            child: Column(
+          children: <Widget>[
+            Title(color: Colors.black, child: Text('Ingredients')),
+            TextField(decoration: InputDecoration(hintText: '250g flour')),
+            TextField(decoration: InputDecoration(hintText: '100ml water')),
+            RaisedButton(child: Text('Add Ingredient'), onPressed: null)
           ],
-        )));
+        ))
+      ],
+    )));
   }
 }
