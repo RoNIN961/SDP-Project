abbf7e2ad2a712c7b46e0a36e286db26110eaf7f
RoNIN961 <75106650+RoNIN961@users.noreply.github.com>
2020-12-29 02:22:25 +0800

    Update settings.dart

--- lib/screens/homescreen/settings.dart
+++ lib/screens/homescreen/settings.dart
@@ -3,12 +3,13 @@ import 'package:flutter/material.dart';
 class SettingsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: RaisedButton(
-      child: Text('Logout'),
-      onPressed: () {
-        Navigator.pushNamed(context, '/');
-      },
-    ));
+    return ListView(children: <Widget>[
+      ListTile(
+        title: Text('logout'),
+        onTap: () {
+          Navigator.pushNamed(context, '/');
+        },
+      )
+    ]);
   }
 }
