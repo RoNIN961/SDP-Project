ad0728ad4d6df5b6e706dc9351c9bfc14c014e18
RoNIN961 <75106650+RoNIN961@users.noreply.github.com>
2020-12-29 03:45:24 +0800

    Update recipe.dart

--- lib/screens/recipe/recipe.dart
+++ lib/screens/recipe/recipe.dart
@@ -14,7 +14,7 @@ class RecipePage extends StatelessWidget {
                   ListTile(
                     leading: ConstrainedBox(
                         constraints:
-                            BoxConstraints(maxWidth: 40.0, maxHeight: 20.0),
+                            BoxConstraints(maxWidth: 80.0, maxHeight: 20.0),
                         child: Image.asset('')),
                     title: Text('Recipe Name'),
                     subtitle: Text('Author Name'),
