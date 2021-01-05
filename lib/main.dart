dc81505e75af68571bddd2d571bd56a58c1f7c20
RoNIN961 <75106650+RoNIN961@users.noreply.github.com>
2021-01-05 15:11:56 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Simple Login Page',
-      initialRoute: '/register',
+      initialRoute: '/',
       theme: ThemeData(
         primarySwatch: Colors.deepOrange,
       ),
