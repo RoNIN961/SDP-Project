b38ad63f677b1b0a6ac49b7b7a6472a532eb684f
RoNIN961 <75106650+RoNIN961@users.noreply.github.com>
2021-01-05 13:29:22 +0800

    Update profile.dart

--- lib/screens/profile/profile.dart
+++ lib/screens/profile/profile.dart
@@ -104,38 +104,6 @@ class ProfilePage extends StatelessWidget {
                 children: [
                   RaisedButton(
                     onPressed: () {
-                      Navigator.pushNamed(context, '/user_recipes');
-                    },
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(80.0),
-                    ),
-                    child: Ink(
-                      decoration: BoxDecoration(
-                        gradient: LinearGradient(
-                            begin: Alignment.centerLeft,
-                            end: Alignment.centerRight,
-                            colors: [Colors.pink, Colors.redAccent]),
-                        borderRadius: BorderRadius.circular(30.0),
-                      ),
-                      child: Container(
-                        constraints: BoxConstraints(
-                          maxWidth: 100.0,
-                          maxHeight: 40.0,
-                        ),
-                        alignment: Alignment.center,
-                        child: Text(
-                          "Recipes",
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 12.0,
-                              letterSpacing: 2.0,
-                              fontWeight: FontWeight.w300),
-                        ),
-                      ),
-                    ),
-                  ),
-                  RaisedButton(
-                    onPressed: () {
                       Navigator.pushNamed(context, '/edit_profile');
                     },
                     shape: RoundedRectangleBorder(
