3fbd06612f194de7e5a981751baa7b8fa87ef90f
RoNIN961 <75106650+RoNIN961@users.noreply.github.com>
2021-01-05 18:08:08 +0800

    Update register.dart

--- lib/screens/authentication/register.dart
+++ lib/screens/authentication/register.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:sdp_project/theme/custom.dart';
 import 'package:http/http.dart' as http;
-import 'dart:async';
 import 'dart:convert';
 
 String username = '';
