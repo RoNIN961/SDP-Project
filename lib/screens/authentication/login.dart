deffbc456b205ba76ae66f8564f1c796c9615757
RoNIN961 <75106650+RoNIN961@users.noreply.github.com>
2021-01-06 04:15:12 +0800

    Update login.dart

--- lib/screens/authentication/login.dart
+++ lib/screens/authentication/login.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:sdp_project/screens/authentication/register.dart';
 import '../../theme/custom.dart';
+import 'dart:async';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 class LoginPage extends StatefulWidget {
   @override
@@ -7,14 +11,61 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  Future userLogin() async {
+    // Getting value from Controller
+    String email = emailController.text;
+    String password = passwordController.text;
+
+    // SERVER LOGIN API URL
+    var url = 'https://flutter-examples.000webhostapp.com/login_user.php';
+
+    // Store all data with Param Name.
+    var data = {'Email': email, 'Password': password};
+
+    // Starting Web API Call.
+    var response = await http.post(url, body: json.encode(data));
+
+    // Getting Server response into variable.
+    var message = jsonDecode(response.body);
+
+    // If the Response Message is Matched.
+    if (message == 'Login Matched') {
+      // Navigate to Profile Screen & Sending Email to Next Screen.
+      Navigator.pushNamed(context, '/Home');
+    } else {
+      // Showing Alert Dialog with Response JSON Message.
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: new Text(message),
+            actions: <Widget>[
+              FlatButton(
+                child: new Text("OK"),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+            ],
+          );
+        },
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     final logo = CustomLogo(onPressed: null, image: null);
     final inputEmail = CustomTextField(
       text: 'email address',
+      controller: emailController,
     );
     final inputPassword = CustomHiddenTextField(
       text: 'password',
+      controller: passwordController,
     );
     final buttonRegister = CustomButton2(
         onPressed: () {
